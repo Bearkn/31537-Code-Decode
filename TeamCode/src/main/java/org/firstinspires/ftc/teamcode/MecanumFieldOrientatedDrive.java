d790b04bd92326e44db9e29774b2298c2b1097dd
Ryan Nguyen <ryantuannguyen07@gmail.com>
2025-10-09 17:27:16 -0500

    auto aim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFieldOrientatedDrive.java
@@ -1,30 +1,70 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.math.MathFunctions;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.mechanism.GoalAprilTagTracker;
 import org.firstinspires.ftc.teamcode.mechanism.MecanumDrive;
+
+import java.math.MathContext;
 
 @TeleOp
 public class MecanumFieldOrientatedDrive extends OpMode {
     MecanumDrive drive = new MecanumDrive();
+    GoalAprilTagTracker tracker = new GoalAprilTagTracker();
+
+    boolean autoaim = false;
+
     double forward, strafe, rotate;
 
     @Override
     public void init(){
         drive.init(hardwareMap);
+        tracker.init(hardwareMap);
     }
 
     @Override
     public void loop(){
-        telemetry.addData("Heading",drive.imu.getHeading(AngleUnit.DEGREES));
+        if(gamepad1.aWasPressed()){
+            autoaim = !autoaim;
+        }
         double y = -gamepad1.left_stick_y;
         double x  = gamepad1.left_stick_x;
         double turn  = gamepad1.right_stick_x;
+        tracker.update();
+        telemetry.addData("Heading", drive.imu.getHeading(AngleUnit.DEGREES));
+        telemetry.addData("x_drive", x);
+        telemetry.addData("y_drive", y);
+        telemetry.addData("t_drive", turn);
+
+        if(!tracker.detections.isEmpty() & autoaim) {
+            telemetry.addData("x", tracker.tag.ftcPose.x);
+            telemetry.addData("y", tracker.tag.ftcPose.y);
+            telemetry.addData("z", tracker.tag.ftcPose.z);
+            telemetry.addData("roll", tracker.tag.ftcPose.roll);
+            telemetry.addData("pitch", tracker.tag.ftcPose.pitch);
+            telemetry.addData("yaw", tracker.tag.ftcPose.yaw);
+
+            if(tracker.tag.ftcPose.x >= 1 || tracker.tag.ftcPose.x <= -1){
+                drive.driveFieldRelative(0,0, MathFunctions.clamp(tracker.tag.ftcPose.x/20,-.4,.4));
+            } else if (Math.abs(tracker.tag.ftcPose.x) < 1 ){
+                if(tracker.tag.ftcPose.y >20){
+                    drive.driveRobotRelative(.2,0,0);
+                }
+                else{
+                    drive.driveRobotRelative(0,0,0);
+                }
+            }
+
+
+        } else {
+            drive.driveFieldRelative(y,x,turn);
+        }
+
 
 //        drive.driveRobotRelative(y,x,turn);
-         drive.driveFieldRelative(y,x,turn);
-
+//         drive.driveFieldRelative(y,x,turn);
     }
 }
