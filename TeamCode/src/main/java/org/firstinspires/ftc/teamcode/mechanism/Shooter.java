774cc3170667a53ef9c6b4f27682d1385d3cddfb
Ryan Nguyen <ryantuannguyen07@gmail.com>
2025-10-31 09:16:14 -0500

    shooter setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Shooter.java
@@ -5,11 +5,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Shooter {
     private DcMotor flyWheelMotor;
+    private DCMotor turretMotor;
 
     public void init(HardwareMap hwMap) {
         flyWheelMotor = hwMap.get(DcMotor.class, "flywheel");
+        turretMotor = hwMap.get(DcMotor.class, "turret");
         flyWheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        turretMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     }
 
